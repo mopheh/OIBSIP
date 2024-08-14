4baefe634242ee7a8b5a9810963090cdf565a847
Mopheh <omomofeadejare257@gmail.com>
2024-08-14 19:49:07 +0100

    fixed bug

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -23,6 +23,7 @@ function App() {
         [name]: value,
       }
     })
+    console.log(task)
   }
 
   useEffect(() => {
@@ -31,10 +32,12 @@ function App() {
 
   function submit(e) {
     e.preventDefault()
-    setTasks([...tasks, task])
+    setTasks([task, ...tasks])
     setTask({
       title: "",
       content: "",
+      time_created: new Date(),
+      status: "Pending",
     })
   }
   const markTask = (taskIndex) => {
@@ -90,66 +93,62 @@ function App() {
         </div>
       </form>
       <div className="mx-auto grid  grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-5 sm:pt-3 lg:mx-0 lg:max-w-none md:grid-cols-3 lg:grid-cols-4">
-        {tasks
-          .slice(0)
-          .reverse()
-          .map((task, index) => (
-            <article
-              key={index}
-              className="flex max-w-2xl mt-5 flex-col items-start bg-white rounded-md px-8 py-10"
-            >
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold  font-salsa leading-6 text-gray-900 ">
-                  <a href="#" className="font-cabin text-2xl">
-                    <span className="absolute inset-0" />
-                    {task.title}
-                  </a>
-                </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 font-ssp text-gray-600">
-                  {task.content}
-                </p>
-              </div>
-              <div className="flex items-center w-full justify-between gap-x-4 mt-2 text-xs">
-                <time className="text-gray-500">
-                  Task Created -{" "}
-                  {new Date(task.time_created).toLocaleString("en-GB")}
-                </time>
-                <a
-                  href="#"
-                  className={`relative z-10 capitalize rounded-full ${
-                    task.status === "Pending"
-                      ? "bg-red-50 text-red-600 hover:bg-red-100"
-                      : "bg-green-50 text-green-600 hover:bg-green-100"
-                  }  px-3 py-1.5 font-medium `}
-                >
-                  Task {task.status}
+        {tasks.map((task, index) => (
+          <article
+            key={index}
+            className="flex max-w-2xl mt-5 flex-col items-start bg-white rounded-md px-8 py-10"
+          >
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold  font-salsa leading-6 text-gray-900 ">
+                <a href="#" className="font-cabin text-2xl">
+                  <span className="absolute inset-0" />
+                  {task.title}
                 </a>
-              </div>
-              <div className="flex items-center w-full justify-between gap-x-4 mt-2 text-xs">
-                <a
-                  href="#"
-                  onClick={() => {
-                    markTask(index)
-                  }}
-                  className="relative z-10 capitalize rounded-full bg-green-50 px-3 py-1.5 font-medium text-green-600 hover:bg-green-100"
-                  disabled={`${task.status === "Completed" && true}`}
-                >
-                  {task.status === "Pending" ? "Mark Complete" : "Completed"}
-                </a>
-                <span> - </span>
-                <time className="text-gray-500">{}</time>
-              </div>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 font-ssp text-gray-600">
+                {task.content}
+              </p>
+            </div>
+            <div className="flex items-center w-full justify-between gap-x-4 mt-2 text-xs">
+              <time className="text-gray-500">
+                Task Created -{" "}
+                {new Date(task.time_created).toLocaleString("en-GB")}
+              </time>
+              <a
+                href="#"
+                className={`relative z-10 capitalize rounded-full ${
+                  task.status === "Pending"
+                    ? "bg-red-50 text-red-600 hover:bg-red-100"
+                    : "bg-green-50 text-green-600 hover:bg-green-100"
+                }  px-3 py-1.5 font-medium `}
+              >
+                Task {task.status}
+              </a>
+            </div>
+            <div className="flex items-center w-full justify-between gap-x-4 mt-2 text-xs">
               <a
                 href="#"
                 onClick={() => {
-                  deleteTask(index)
+                  markTask(index)
                 }}
-                className="relative z-10 capitalize rounded-full bg-red-50 px-3 py-1.5 font-medium ml-auto mt-3  text-xs hover:bg-red-100"
+                className="relative z-10 capitalize rounded-full bg-green-50 px-3 py-1.5 font-medium text-green-600 hover:bg-green-100"
               >
-                <TrashIcon className="h-5 w-5 text-red-600" />
+                {task.status === "Pending" ? "Mark Complete" : "Completed"}
               </a>
-            </article>
-          ))}
+              <span> - </span>
+              <time className="text-gray-500">{}</time>
+            </div>
+            <a
+              href="#"
+              onClick={() => {
+                deleteTask(index)
+              }}
+              className="relative z-10 capitalize rounded-full bg-red-50 px-3 py-1.5 font-medium ml-auto mt-3  text-xs hover:bg-red-100"
+            >
+              <TrashIcon className="h-5 w-5 text-red-600" />
+            </a>
+          </article>
+        ))}
       </div>
     </div>
   )
